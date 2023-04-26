3fcea07673d748e75b0a154025756b7a3acc3738
FelixOnyangoh1 <felix.onyangoh1@gmail.com>
2023-04-26 10:38:44 +0300

    update-4

---
+++ string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * _strcpy - this copies string
+ * @dest: destination file
+ * @src: source file
+ *
+ * Return:returns a pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - this duplicates a string
+ * @str: string to be duplicated
+ *
+ * Return: the pointer to a duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - this prints the input string
+ * @str: string to get printed
+ *
+ * Return: Null
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - modifies the char c to the stdout
+ * @c: char to print
+ *
+ * Return: returns 1 On success
+ * otherwise, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
